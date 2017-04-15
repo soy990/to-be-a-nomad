98a987a29422efdecc3a71e8376e868ffc602f69
Sehun Park <blueade@Sehunui-Mac-mini.local>
2017-04-15 16:57:15 +0900

    Add basic operators

---
+++ 01_basicOperators.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//str
+
+// variable 변수
+var number1 = 1
+//number1
+
+// 상수
+
+let number2 = 2
+//number2 = 4
+
+1 + 2
+5 - 4
+2 * 3
+10.0 / 2.5
+
+10/3
+10%3
+
+
+"abc" + "def"
+
+var number3 = -1
+
+number3 += 3
