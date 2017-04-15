bfaca733c5b0b5cc60114e7b7dc406311ae0d8a4
Sehun Park <blueade@Sehunui-Mac-mini.local>
2017-04-15 17:16:41 +0900

    add dataTypes

---
+++ 02_dataTypes.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var num = 1
+
+// explicit 방법으로 코딩하는 것이 좋음.
+var str2:String = "hello"
+var num2:Int = 3
+
+// convert string to integer
+10+12
+"10"+"12"
+//"10"+12
+
+// 문자형태를 숫자로 바꿔주는법
+var numStr = "100"
+Int(numStr)
+
+// 숫자를 문자로 바꾸는법
+var num3 = 5
+var str3 = "tired"
+
+"\(num3)"
+"\(str3)\(num3)"
+String(num3)
+
+Int(3.00/2.00)
+
+
+
+
+
+
+
