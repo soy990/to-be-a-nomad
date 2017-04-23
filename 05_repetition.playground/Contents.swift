cff67b2ffc28b6e959d6f22a0178340574551f67
Sehun Park <blueade@Sehunui-Mac-mini.local>
2017-04-23 12:20:41 +0900

    Repetition

---
+++ 05_repetition.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+
+
+for index in 1...5{
+    print(index)
+}
+
+
+for _ in 1...100 {
+    print("for statement")
+}
+
+
+let names = ["Jane", "Stacy", "Kim", "Terry"]
+for name in names {
+    print("Hello, \(name)!")
+}
+
+var num = 0
+
+while (num <= 10) {
+    print("Chao! \(num)")
+    num+=1
+}
